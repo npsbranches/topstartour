5fef52acf5baf036893d6a066dacc51a38a274cb
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-04-28 23:04:12 +0500

    pages build 1.2

--- js/script.js
+++ js/script.js
@@ -82,22 +82,24 @@ $(function () {
     });
 })
 
-$('.attractions').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 4
-        }
-    }
-});
+$(window).on('load', function () {
 
+    $('.attractions').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
 
 $('.attractions-view').magnificPopup();
