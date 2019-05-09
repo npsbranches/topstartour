0ac2842b8fa02f57caf520dd0ed755010d1d13cd
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-05-09 17:53:29 +0500

    gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+$('.zoom-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+            return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+        }
+    },
+    gallery: {
+        enabled: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300, // don't foget to change the duration also in CSS
+        opener: function(element) {
+            return element.find('img');
+        }
+    }
+    
+});
+
 $('.home-hero-slider').slick({
     dots: false,
     infinite: true,
@@ -6,6 +31,7 @@ $('.home-hero-slider').slick({
     cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 5000,
+    arrows: false,
   });
       
 
@@ -153,3 +179,5 @@ $(window).on('load', function () {
 $('.attractions-view').magnificPopup();
 $('.tour').magnificPopup();
 $('.home-news-link').magnificPopup();
+
+
